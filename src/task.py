e6fd8e0e8e5a200b4e8427e7a5169b0a0d25ba00
justinphan3110 <justinphan3110@gmail.com>
2023-01-30 12:56:52 -0500

    update extension for registerTassk

--- src/task.py
+++ src/task.py
@@ -14,22 +14,28 @@ DEFAULT_OUTPUT_FEATURES = {
         vocabulary=t5.data.get_default_vocabulary(), add_eos=True)
 }
 
-def registerTask(task: str, splits: Dict, metric_name: str="PRF1"):
+def registerTask(task: str, splits: Dict, metric_name: str="PRF1", extension: str="tsv"):
   def parseDataset(split: str, shuffle_files: bool = False, seed: int = 0):
     ds = tf.data.TextLineDataset([splits[split]])
-    ds = ds.map(
-    functools.partial(tf.io.decode_csv, record_defaults=["", ""],
-                      field_delim="\t", use_quote_delim=False),
-    num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    ds = ds.map(lambda *ex: dict(zip(["input", "target"], ex)))
+
+    if extension == "tsv":
+      ds = ds.map(
+      functools.partial(tf.io.decode_csv, record_defaults=["", ""],
+                        field_delim="\t", use_quote_delim=False),
+      num_parallel_calls=tf.data.experimental.AUTOTUNE)
+      ds = ds.map(lambda *ex: dict(zip(["input", "target"], ex)))
     return ds
   def preprocessor(ds):
     def to_inputs_and_targets(ex):
-      """Map {"inputs": ..., "targets": ...}->{"inputs": {task}..., "targets": ...}."""
-      return {
-          "inputs":ex["input"],
-          "targets": ex["target"]
-      }
+      if extension == "tsv":
+        return {
+            "inputs":ex["input"],
+            "targets": ex["target"]
+        }
+      else:
+        return {
+          "inputs":ex["input"]
+        }
     return ds.map(to_inputs_and_targets, 
                   num_parallel_calls=tf.data.experimental.AUTOTUNE)
   
